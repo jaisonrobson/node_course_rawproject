737c1341511a5f520ba9a4cbc9247d8863b4c5ec
jaisonrobson <xd_jrg@hotmail.com>
2023-05-20 01:14:19 -0300

    feat: adicionado tratamento para rotas de exemplo usando o express

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ var express = require('express');
 
 var app = express();
 
+app.get('/', function(req, res) {
+    res.send('<html><body>Portal de noticias</body></html>');
+})
+
+app.get('/', function(req, res) {
+    res.send('<html><body>Noticias de tecnologia</body></html>');
+})
+
 app.listen(3000, function() {
     console.log("Servidor rodando com express...")
 });
