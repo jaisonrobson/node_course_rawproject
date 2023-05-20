75c3dad5eec51abcff7892ad11a17a229c404604
jaisonrobson <xd_jrg@hotmail.com>
2023-05-20 01:53:14 -0300

    feat: ajustado configuracao do servidor para interagir com modularizacao das rotas & chamado todas as rotas em app.js de forma modularizada

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
 var app = require('./config/server');
 
-app.get('/', function(req, res) {
-    res.render("home/index");
-})
-
-app.get('/tecnologia', function(req, res) {
-    res.render("secao/tecnologia");
-})
-
-app.get('/formulario_inclusao_noticia', function(req, res) {
-    res.render("admin/form_add_noticia");
-})
-
-app.get('/noticias', function(req, res) {
-    res.render("noticias/noticias");
-})
+var rotaNoticias = require("./app/routes/noticias")(app);
+var rotaHome = require("./app/routes/home")(app);
+var rotaInclusao = require("./app/routes/formulario_inclusao_noticia")(app);
 
 app.listen(3000, function() {
     console.log("Servidor rodando com express...");
