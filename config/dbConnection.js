8122eedc161bc796a1653c43a2feba658b45b647
jaisonrobson <xd_jrg@hotmail.com>
2023-05-21 18:42:00 -0300

    feat: aplicado tecnica de wrapping na configuracao de conexao com banco de dados & incluido conexao com banco de dados no consign que posteriormente injeta essa conexao na variavel app que representa o servidor & refatorado arquivo de rota noticias.js para utilizar codigo de conexao injetado pelo consign

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,5 +1,7 @@
-module.exports = function() {
-    var mysql = require('mysql');
+var mysql = require('mysql');
+
+var mysqlConnection = function() {
+    console.log('Conexao com banco de dados estabelecida.');
 
     return mysql.createConnection({
         host: 'localhost',
@@ -7,4 +9,10 @@ module.exports = function() {
         password: '1234',
         database: 'portal_noticias'
     });
+}
+
+module.exports = function() {
+    console.log('Autoload carregou modulo de conexao com banco de dados.');
+
+    return mysqlConnection;    
 }
