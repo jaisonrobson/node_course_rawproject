8709bbcd3d062f4af8630c525f17dcdb23cd0ff3
jaisonrobson <xd_jrg@hotmail.com>
2023-05-23 23:36:44 -0300

    feat: feito validacao de dados enviados do formulario de inclusao de noticia antes de fazer a conexao com o backend & feito tratamento para nao prosseguir caso haja erros no formulario

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -6,6 +6,21 @@ module.exports = function (app) {
     app.post('/noticias/salvar', function(req, res) {
         var noticia = req.body;
 
+        req.assert('titulo', 'Título é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
+        req.assert('autor', 'Autor é obrigatório').notEmpty();
+        req.assert('data_noticia', 'Data é obrigatório').notEmpty().isDate({ format: 'YYYY-MM-DD' });
+        req.assert('noticia', 'Noticia é obrigatório').notEmpty();
+
+        var errors = req.validationErrors();
+
+        if (errors) {
+            res.render("admin/form_add_noticia");
+
+            return;
+        }
+
         var connection = app.config.dbConnection();
         var noticiasDAO = new app.app.models.NoticiasDAO(connection);
 
