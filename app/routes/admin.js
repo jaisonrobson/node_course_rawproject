11f7048b46844c3a547b5b9f33e4560fdac87f49
jaisonrobson <xd_jrg@hotmail.com>
2023-05-24 23:26:43 -0300

    feat: criado controllers no projeto & refatorado devido codigo das rotas para os controllers

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,31 +1,9 @@
 module.exports = function (app) {
     app.get('/formulario_inclusao_noticia', function(req, res) {
-        res.render("admin/form_add_noticia", { validacao: {}, noticia: {} });
+        app.app.controllers.admin.formulario_inclusao_noticia(app, req, res);
     })
 
     app.post('/noticias/salvar', function(req, res) {
-        var noticia = req.body;
-
-        req.assert('titulo', 'Título é obrigatório').notEmpty();
-        req.assert('resumo', 'Resumo é obrigatório').notEmpty();
-        req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
-        req.assert('autor', 'Autor é obrigatório').notEmpty();
-        req.assert('data_noticia', 'Data é obrigatório').notEmpty().isDate({ format: 'YYYY-MM-DD' });
-        req.assert('noticia', 'Noticia é obrigatório').notEmpty();
-
-        var errors = req.validationErrors();
-
-        if (errors) {
-            res.render("admin/form_add_noticia", { validacao: errors, noticia: noticia });
-
-            return;
-        }
-
-        var connection = app.config.dbConnection();
-        var noticiasDAO = new app.app.models.NoticiasDAO(connection);
-
-        noticiasDAO.salvarNoticia(noticia, function(error, result) {
-            res.redirect('/noticias');
-        });
+        app.app.controllers.admin.noticias_salvar(app, req, res);
     })
 }
