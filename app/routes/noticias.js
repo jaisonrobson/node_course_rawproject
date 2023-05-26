d1a1d0bfae1ce7f675916fd4d6ec5ba781d14b94
jaisonrobson <xd_jrg@hotmail.com>
2023-05-26 18:40:00 -0300

    fix: ajustado rota de noticia e noticias & substituido views por conteudo estatico de htmls com estrutura do website de noticias

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,9 +1,9 @@
 module.exports = function(app) {
     app.get('/noticias', function(req, res) {
-        app.app.controllers.admin.noticias(app, req, res);
+        app.app.controllers.noticias.noticias(app, req, res);
     })
 
     app.get('/noticia', function(req, res) {
-        app.app.controllers.admin.noticia(app, req, res);
+        app.app.controllers.noticias.noticia(app, req, res);
     })
 }
